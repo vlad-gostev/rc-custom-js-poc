ba0a920767e340237d94e0337565fe56f397c887
Vlad Gostev <45634333+vlad-gostev@users.noreply.github.com>
2023-06-06 12:01:12 +0200

    Create hide-rooms-by-business-id.js

---
+++ hide-rooms-by-business-id.js
@@ -0,0 +1,53 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// Passed business id in iframe for example
+const CLINIC_ID = urlParams.get('businessId')
+
+// Action to avoid initial state view
+document.getElementById('react-root').style.visibility = 'hidden'
+
+// Entire DOM interface is rendered in some time after custom script is run so we need postpone main DOM processing
+setTimeout(() => {
+    console.log('start')
+    document.getElementById('react-root').style.visibility = 'visible'
+
+    const sidebar = document.getElementById('sidebar-region')
+
+    if (!CLINIC_ID) {
+        return false
+    }
+
+    const handleBusinessId = () => {
+        document.querySelectorAll('.rcx-sidebar-item').forEach(el => {
+            const title = el.querySelector('.rcx-sidebar-item__title').textContent
+            if (!title.endsWith(CLINIC_ID)) {
+                el.style.visibility = 'hidden'
+                // Inner virtual list lib has conflict if we hide element entirely
+                el.style.height = '1px'
+                el.style.padding = '0px'
+            }
+        })
+    }
+
+    handleBusinessId()
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            let oldValue = mutation.oldValue;
+            let newValue = mutation.target.textContent;
+            if (oldValue !== newValue) {
+                console.log('mutation')
+                handleBusinessId()
+            }
+        });
+    });
+
+    // Such config causes much processing calls so that may overload with heavy state updates
+    observer.observe(sidebar, {
+        characterDataOldValue: true,
+        subtree: true,
+        childList: true,
+        characterData: true
+    });
+}, 1000)
